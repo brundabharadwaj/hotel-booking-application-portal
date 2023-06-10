b9bf550334ac881f9e47ec17dbd25ec31d0bfe19
Adarsh D <adarsh.d@spurtreetech.in>
2023-06-10 17:04:42 +0530

    Update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,13 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { ROUTES } from "./constants/routes";
-import { getSessionStorageItem } from "./constants/sessions";
-import { TOKENS } from "./constants/textConstant";
 import { AuthRoute } from "./core/AuthRoutes";
 import Login from "./screens/Auth/Login";
 import Registration from "./screens/Auth/Registration";
-import { useAppDispatch } from "./redux/hooks";
-import { setUserDataFromSessions } from "./redux/auth";
 import PageNotFound from "./screens/404/PageNotFound";
 import "./App.css";
 
 function App() {
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    let user = getSessionStorageItem(TOKENS.sessionStorageToken);
-    dispatch(setUserDataFromSessions(user));
-  }, [dispatch]);
-
   return (
     <>
       <Routes>
@@ -34,7 +23,7 @@ function App() {
             </AuthRoute>
           }
         />
-
+        
         <Route path={"*"} element={<PageNotFound />} />
       </Routes>
     </>
